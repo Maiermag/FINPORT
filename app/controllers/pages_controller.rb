6fee04218a4779132bb9ae2ac5b07670f8c17da9
maiermag <maiermag@gmail.com>
2021-03-02 18:04:24 +0100

    controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def portfolio_import
+    @portfolio = Portfolio.find(params[:id])
+    @etf_assets = @portfolio.assets.where(asset_category: "ETF")
+    @share_assets = @portfolio.assets.where(asset_category: "share")
+    @crypto_assets = @portfolio.assets.where(asset_category: "cryptocurrency")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/portfolio_import", to: 'pages#portfolio_import', as: :portfolio_import
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :portfolios, only: [:index, :show]
 end
+
