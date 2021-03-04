4196496e10e53b31ce1e5effea4880d3309ad7cf
maiermag <maiermag@gmail.com>
2021-03-04 18:42:39 +0100

    Saved changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,20 @@ class User < ApplicationRecord
     end
   end
 
+  def total_invested_asset_value(asset_name)
+    assets.where(asset_name: asset_name).sum do |asset|
+      asset.total_invested_value
+    end
+  end
+
+  def asset_categories
+    assets.pluck(:asset_category).sort
+  end
+
+  def total_category_current_value(asset_category)
+    assets.where(asset_category: asset_category).sum do |asset|
+      asset.total_current_value
+    end
+  end
+
 end
