9e7e23a7857ae1fca56fe0cad83a3f027e9bef32
maiermag <maiermag@gmail.com>
2021-03-05 14:22:31 +0100

    last changes to calculations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -39,10 +39,6 @@ class Portfolio < ApplicationRecord
   end
 
 
-  # def industry_names
-  #   industries.pluck(:name).sort
-  # end
-
   def total_current_industry_value(industry)
     assets.where(industry: industry).sum do |asset|
       asset.total_current_value
@@ -53,6 +49,30 @@ class Portfolio < ApplicationRecord
     assets.where(industry: industry).sum do |asset|
       asset.total_invested_value
     end
+  end
+
+  def total_current_portfolio_value
+    assets.sum do |asset|
+      asset.total_current_value
+    end
+  end
+
+  def asset_names
+     assets.pluck(:asset_name)
+  end
+
+  # array of hashes:
+  def assets_by_name(sort_key = :portfolio_share)
+    hashes = asset_names.uniq.map do |asset_name|
+      current_value = total_current_specific_asset_value(asset_name)
+      {
+        name: asset_name,
+        portfolio_value: current_value,
+        portfolio_share: ((current_value / total_current_portfolio_value) * 100),
+        performance: (((total_current_specific_asset_value(asset_name)/ total_initial_specific_asset_value(asset_name)) * 100) - 100)
+      }
+    end
+    hashes.sort{ |a,b| b[sort_key] <=> a[sort_key]}
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,48 @@ class User < ApplicationRecord
     assets.pluck(:asset_name).sort
   end
 
+  # array of hashes:
+  def assets_by_name(sort_key = :portfolio_share)
+    hashes = asset_names.uniq.map do |asset_name|
+      current_value = total_current_specific_asset_value(asset_name)
+      {
+        name: asset_name,
+        portfolio_value: current_value,
+        portfolio_share: ((current_value / total_invest_current_value) * 100),
+        performance: (((current_value / total_invested_asset_value(asset_name)) * 100) - 100)
+      }
+    end
+    hashes.sort{ |a,b| b[sort_key] <=> a[sort_key]}
+  end
+
+  def industry_names
+    industries.pluck(:name)
+  end
+
+  def industries_by_name(sort_key = :portfolio_share)
+  hashes = industries.uniq.map do |industry|
+    current_value = total_current_asset_value(industry)
+    {
+      name: industry.name.capitalize,
+      portfolio_value: current_value,
+      portfolio_share: ((current_value / total_invest_current_value) * 100),
+    }
+    end
+    hashes.sort{ |a,b| b[sort_key] <=> a[sort_key]}
+  end
+
+  def categories_by_name(sort_key = :portfolio_share)
+  hashes = asset_categories.uniq.map do |asset_category|
+    current_value = total_category_current_value(asset_category)
+    {
+      name: asset_category.capitalize,
+      portfolio_value: current_value,
+      portfolio_share: ((current_value / total_invest_current_value) * 100),
+    }
+    end
+    hashes.sort{ |a,b| b[sort_key] <=> a[sort_key]}
+  end
+
   def total_current_asset_value(industry)
     portfolios.sum do |portfolio|
       portfolio.total_current_asset_value(industry)
